1965464fbecc03b9f452fd2549c231e55934e599
Udara Bibile <bibile.13@cse.mrt.ac.lk>
2021-12-08 14:55:26 +0530

    platform service

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using( var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("SEEDING DATA...");
+                context.Platforms.AddRange(
+                    new Platform() { Name="Dotnet", Publisher="Microsoft", Cost="Free" },
+                    new Platform() { Name="SqlServer", Publisher="Microsoft", Cost="Free" },
+                    new Platform() { Name="Kuberenetes", Publisher="CloudNative", Cost="Free" }
+                );
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("ALREADY HAVE DATA");
+            }
+        }
+    }
+}
